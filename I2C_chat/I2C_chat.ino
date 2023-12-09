4d1219db1e439215b2f84f3eb5970b6c9b7e2427
Sonya Buzevich <sonyabuzevich@gmail.com>
2023-12-09 16:47:37 +0800

    чатик

--- I2C_chat/I2C_chat.ino
+++ I2C_chat/I2C_chat.ino
@@ -2,6 +2,7 @@
 
 int messFrom = 0;
 unsigned char id = 1;
+int lastSenderAddress = -1;
 
 struct AddressTable {
   char name;
@@ -15,7 +16,7 @@ AddressTable addressTable[] = {
 };
 
 void setup() {
-  Serial.begin(9600);   
+  Serial.begin(9600);
   Wire.begin(id);
   Wire.onReceive(receiveEvent);
 }
@@ -24,54 +25,79 @@ unsigned long t = millis();
 
 void loop() {
   if (Serial.available()) {
-    char recipientName = Serial.read();
-    int recipientAddress = -1;
-    for (int i = 0; i < sizeof(addressTable) / sizeof(addressTable[0]); i++) {
-      if (addressTable[i].name == recipientName) {
-        recipientAddress = addressTable[i].address;
-        break;
-      }
-    }
+    char inputChar = Serial.read();
+    
+    if (inputChar == 'a') {
+      if (lastSenderAddress != -1) {
+        Serial.print("Responding to the last sender\n");
+        Serial.print("Message: ");
 
-    if (recipientAddress != -1) {
-      Serial.print("Addressee's name: ");
-      Serial.write(recipientName);
-      Serial.write("\n");
-      Serial.print("Message: ");
-      
-      Wire.beginTransmission(recipientAddress);
-      delay(100);
-      
-      Wire.write(id);
-      while (Serial.available()) {
-        char c = Serial.read();
-        Serial.write(c);
-        Wire.write(c);
+        Wire.beginTransmission(lastSenderAddress);
+        delay(100);
+
+        Wire.write(id);
+        while (Serial.available()) {
+          char c = Serial.read();
+          Serial.write(c);
+          Wire.write(c);
+        }
+
+        Wire.write('.');
+
+        Wire.endTransmission();
+        Serial.write("\nMessage delivered as a response.\n");
+      } else {
+        Serial.println("No previous sender to respond to.\n");
       }
-      
-      Wire.write('.');
-      
-      Wire.endTransmission();
-      Serial.write("\nMessage delivered.\n");
     } else {
-      Serial.println("Recipient not found in the address table.\n");
+      int recipientAddress = -1;
+      for (int i = 0; i < sizeof(addressTable) / sizeof(addressTable[0]); i++) {
+        if (addressTable[i].name == inputChar) {
+          recipientAddress = addressTable[i].address;
+          break;
+        }
+      }
+
+      if (recipientAddress != -1) {
+        Serial.print("Addressee's name: ");
+        Serial.write(inputChar);
+        Serial.write("\n");
+        Serial.print("Message: ");
+        
+        Wire.beginTransmission(recipientAddress);
+        delay(100);
+        
+        Wire.write(id);
+        while (Serial.available()) {
+          char c = Serial.read();
+          Serial.write(c);
+          Wire.write(c);
+        }
+        
+        Wire.write('.');
+        
+        Wire.endTransmission();
+        Serial.write("\nMessage delivered.\n");
+      } else {
+        Serial.println("Recipient not found in the address table.\n");
+      }
     }
   }
 }
 
 void receiveEvent(int length) {
   messFrom = Wire.read();
-  
+  lastSenderAddress = messFrom;
   Serial.write("Sender's name: ");
   Serial.write(addressToName(messFrom));
   Serial.write("\n");
   Serial.write("Message: ");
-  
+
   while (Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
-  }  
-  Serial.println();     
+  }
+  Serial.println();
 }
 
 char addressToName(int addr) {
